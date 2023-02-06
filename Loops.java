0a75a94d096574160511c81bdf4e07c9a8f41248
Atley St. Vrain <atleystvrain@gmail.com>
2023-02-06 11:49:48 -0600

    nHellos | Complete

--- Loops.java
+++ Loops.java
@@ -1,10 +1,27 @@
 public class Loops {
 
   // static methods go here
+  public static void nHellos(int n) {
+    for (int index = 1; index < n; index++) {
+      if (index % 100 == 11 || index % 100 == 12 || index % 100 == 13) {
+        System.out.println(index+"th hello");
+      } else if (index % 10 == 1) {
+        System.out.println(index+"st hello");
+      } else if (index % 10 == 2) {
+        System.out.println(index+"nd hello");
+      } else if (index % 10 == 3) {
+        System.out.println(index+"rd hello");
+      } else {
+        System.out.println(index+"th hello");
+      }
+      }
+    }
+    public static void main(String[] args) {
 
-  public static void main(String[] args) {
-
-    // testing of static methods goes here
-
+      // testing of static methods goes here
+  
+      nHellos(125);
+    }
   }
-}
+
+  
